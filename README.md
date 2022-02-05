54521c8bba01bc06cec98c8ab520c9d8d1239d78
Anniek <anniekgithub@gmail.com>
2022-02-05 13:22:54 +0100

    finished filter

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # js-search-filter
 
 User card data are pulled in via API
+https://jsonplaceholder.typicode.com/users
 
 HTML uses a template. Allows you to create html that doesn't show up on the page. 
--- main.js
+++ main.js
@@ -4,11 +4,19 @@ const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardsContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
+// empty array to start with
+let users =[]
 // runs every time input is changed
 // e -> want to get the value of the input
 searchInput.addEventListener('input', (e) =>{
- const value = e.target.value
- console.log(value)
+    //convert everything to lowecase to make checking easier
+ const value = e.target.value.toLowerCase()
+ //does it match input? Yes? -> No? -> Hide
+ users.forEach(user => {
+     const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
+     user.element.classList.toggle('hide', !isVisible)
+ })
+ 
 })
 
 
@@ -16,7 +24,7 @@ searchInput.addEventListener('input', (e) =>{
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(res => res.json())
 .then(data=> {
-    data.forEach(user => {
+  users = data.map(user => {
     // get the content inside our template and clone the content. Returns a document fragment. Tu use the content in the
     // fragement you need to get the first child. -> .children [0]
     const card = userCardTemplate.content.cloneNode(true).children[0]
@@ -25,6 +33,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
     header.textContent = user.name
     body.textContent = user.email
     userCardsContainer.append(card) 
+        //return object -> contains data about user
+    return{
+        name:user.name, email: user.email, element: card
+    }
     });
 
 })
